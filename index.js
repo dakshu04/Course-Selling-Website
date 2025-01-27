60bfb155000ca1d5fa0edce5601b9f26434d5903
Daksh Purohit <dakshgrows@gmail.com>
2025-01-27 23:03:42 +0530

    Step 3 - Added routers

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const { userRouter } = require("./routes/user")
+const { createCourseRoutes } = require("./routes/course")
+const app = express();
 
-app.post('/user/signup', (req, res) => {
-    res.json({
-        message: "signup endpoint"
-    })
-})
+app.use("/user", userRouter)
+app.use("course", createCourseRoutes)
 
-app.post('/user/signin', (req, res) => {
-    res.json({
-        message: "signin endpoint"
-    })
-})
-
-app.get('/user/purchases', (req, res) => {
-    res.json({
-        message: "user/purchases"
-    })
-})
-
-app.post('/course/purchase', (req, res) => {
-    //you would expect the user to pay you money
-    res.json({
-        message: "course/purchases"
-    })
-})
-
-app.get('/courses', (req, res) => {
-    res.json({
-        message: "courses"
-    })
-})
+createUserRoutes(app);
+createCourseRoutes(app);
 
 app.listen(port, ()=> {
     console.log(`Example app listening on port ${port}`);
